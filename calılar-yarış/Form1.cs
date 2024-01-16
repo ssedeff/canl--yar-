4f8b7b1ee266a78554e00c74ed83fc66e1afba07
ssedeff <145463803+ssedeff@users.noreply.github.com>
2024-01-16 10:54:27 +0300

    Add files via upload

---
+++ calılar-yarış/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calılar_yarış
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            button1.Left = button1.Left + 10 + random.Next(10, 30);
+            button2.Left = button2.Left + 10 + random.Next(10, 30);
+            button3.Left = button3.Left + 10 + random.Next(10, 30);
+            button4.Left = button4.Left + 10 + random.Next(10, 30);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            button1.Left = 0;
+            button2.Left = 0;
+            button3.Left = 0;
+            button4.Left = 0;
+        }
+        
+    }
+}
